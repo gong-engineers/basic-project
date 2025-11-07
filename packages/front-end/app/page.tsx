b10772eaa7ffc3f6fd99cd8ae6de12ae35a0c37d
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2025-11-07 19:48:00 +0900

    장바구니 UI만 우선 구성

--- packages/front-end/app/page.tsx
+++ packages/front-end/app/page.tsx
@@ -9,18 +9,18 @@ export default function Home() {
 
   useEffect(() => {
     // 로그인 기능 합치기 이전까지의 데스트 로그인 api 호출
-  //   const response = client.post('http://localhost:3001/auth/login', {
-  //     email: 'test@gmasil.com',
-  //     password: 'testtest',
-  //   },
-  //   {
-  //     mode: "cors",                // CORS 요청 허용
-  // credentials: "include",      // 쿠키 포함
-  // headers: {
-  //   "Content-Type": "application/json",
-  // },
-  //   });
-  //   console.log(response);
+    //   const response = client.post('http://localhost:3001/auth/login', {
+    //     email: 'test@gmasil.com',
+    //     password: 'testtest',
+    //   },
+    //   {
+    //     mode: "cors",                // CORS 요청 허용
+    // credentials: "include",      // 쿠키 포함
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
+    //   });
+    //   console.log(response);
     // router.push('/carts');
   }, []);
 
