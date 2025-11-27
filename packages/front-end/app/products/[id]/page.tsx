2da6f7531f7d00e08a7c2f51765c4932c6867971
elinhyerp <parkhr0225@gmail.com>
2025-11-27 15:02:12 +0900

    feat: Add metadata

--- packages/front-end/app/products/[id]/page.tsx
+++ packages/front-end/app/products/[id]/page.tsx
@@ -1,10 +1,24 @@
 import { client } from '@/lib/api';
 import { item } from '@basic-project/shared-types';
+import { Metadata } from 'next';
 import DescriptionSection from './components/DescriptionSection';
 import ImageSection from './components/ImageSection';
 
 interface Props {
   params: Promise<{ id: string }>;
+}
+
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const resolvedParams = await props.params;
+
+  const product = await client.get<null, item.Product>(
+    `${process.env.API_URL}/api/v1/products/${resolvedParams.id}`,
+  );
+
+  return {
+    title: `SportHub - ${product.name}`,
+    description: product.description,
+  };
 }
 
 async function ProductPage(props: Props) {
