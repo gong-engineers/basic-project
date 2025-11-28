ecdccae87089a4adf79ca05d3240788a6c099b3d
NSRBSG <nsrbsg@gmail.com>
2025-11-28 15:04:03 +0900

    chore: 더미데이터 추가

--- packages/back-end/src/seed/data/users.data.ts
+++ packages/back-end/src/seed/data/users.data.ts
@@ -4,16 +4,6 @@ import { DeepPartial } from 'typeorm';
 export const USERS_DATA: DeepPartial<User>[] = [
   {
     id: 1,
-    email: 'test.user.1@example.com',
-    password: 'password123',
-  },
-  {
-    id: 2,
-    email: 'test.user.2@example.com',
-    password: 'password456',
-  },
-  {
-    id: 3,
     email: 'admin@example.com',
     password: 'adminPassword',
     role: 'ADMIN',
